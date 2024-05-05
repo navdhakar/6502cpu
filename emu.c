f89eaebccda56bc7e67d90bf892b1b0d667e9116
Navdeep Dhakar <navdepdhakar1@gmail.com.com>
2024-05-05 16:49:07 +0530

    started working on keyboard input

--- emu.c
+++ emu.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "MCS6502.h"
 
 #define MEMORY_SIZE (64 * 1024)
 typedef unsigned char Byte;
 typedef unsigned short Word;
 
+#define KBD 0xD010 
+#define KBDCR 0xD011
+#define DSP 0xD012
+#define DSPCR 0xD013
 typedef struct{
     Byte Data[MEMORY_SIZE];
 }Mem;
@@ -25,6 +32,11 @@ void mem_write(Word address, Byte value, void *readWriteContext) {
 void write_code(Mem* memory){
 	mem_write(0x0200, 0xA9, memory);
 	mem_write(0x0200+1, 0x42, memory);
+}
+void pma(Word address, Mem* memory){
+
+	Byte rd = mem_read(address, memory);
+	printf("data at address 0x%02X -> 0x%02X\n", rd); 
 }
 void load_rom_file(const char *filename, Word startAddress, Mem *memory) {
     FILE *file = fopen(filename, "rb");
@@ -55,6 +67,76 @@ void display(Mem* memory){
 		lastValue = rd;
 	}
 
+}
+int kbhit(void) {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+char getch() {
+    char buf = 0;
+    struct termios old = {0};
+
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if (read(0, &buf, 1) < 0)
+        perror("read()");
+
+    return buf;
+}
+void keyboard_polling(Mem* memory){
+    if (kbhit()) // Checking whether any key is pressed or not
+    {
+        char key = getch(); // get the character from keyboard buffer
+        // converting lower case character to upper case because wozmon accept only uppercase
+        if(key >= 97 && key <= 122)
+            key = key-32;
+        // wozmon echo only if values lies in this range
+        // lower case =  97  to  122
+        // upper case =  65  to  90
+        // numbers 0 - 9  =  48  to  57
+        // Enter = 13
+        // space = 32
+        if((key >= 65 && key <= 90) || (key >= 48 && key <= 57) || key == 13 || key == 32){
+            // base is 0x80 to convert it to something wozmon echo can understand
+            //write key to keyboard
+            mem_write(KBD, key+0x80, memory);
+            //set bit 7 high => keyboard is ready for reading
+            mem_write(KBDCR, 0x80, memory);
+			pma(KBD, memory);
+            //set bit 7 high => keyboard is ready for reading
+
+            //set 'DA' => getting ready for echo from wozmon
+            //increment last value by 0x80 because DA should be set
+            //as echo routine waits for DA to get cleared
+        }
+    }
 }
 
 int main (int argc, int *argv[]){
@@ -103,7 +185,6 @@ int main (int argc, int *argv[]){
             cycleCount++;
 			display(memory);
         }
-        MCS6502Tick(&context);
     }
 
     free(memory);
